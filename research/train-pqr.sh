dca23ab541e9349bf4d39c72080bdca357737135
shane-combs-gdls <combss1@gdls.com>
2022-10-25 09:17:11 -0400

    Train update

--- research/train-pqr.sh
+++ research/train-pqr.sh
@@ -6,14 +6,13 @@ DATASET_DIR="datasets"
 # Set up the working directories.
 PQR_FOLDER="PQR"
 EXP_FOLDER="exp/train_on_trainval_set"
-INIT_FOLDER="${WORK_DIR}/${DATASET_DIR}/${PQR_FOLDER}/${EXP_FOLDER}/init_models"
 TRAIN_LOGDIR="${WORK_DIR}/${DATASET_DIR}/${PQR_FOLDER}/${EXP_FOLDER}/train"
 DATASET="${WORK_DIR}/${DATASET_DIR}/${PQR_FOLDER}/tfrecord"
 
 mkdir -p "${WORK_DIR}/${DATASET_DIR}/${PQR_FOLDER}/exp"
 mkdir -p "${TRAIN_LOGDIR}"
 
-NUM_ITERATIONS=9000
+NUM_ITERATIONS=3000
 python3 "${WORK_DIR}"/train.py \
   --logtostderr \
   --train_split="train" \
@@ -27,6 +26,5 @@ python3 "${WORK_DIR}"/train.py \
   --train_batch_size=4 \
   --training_number_of_steps="${NUM_ITERATIONS}" \
   --fine_tune_batch_norm=true \
-  --tf_initial_checkpoint="${INIT_FOLDER}/deeplabv3_pascal_train_aug/model.ckpt" \
   --train_logdir="${TRAIN_LOGDIR}" \
   --dataset_dir="${DATASET}"
