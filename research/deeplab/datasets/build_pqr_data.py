03fc3b10cff0f9625d348f255e2bffebe90b3db3
scombs97 <61484087+scombs97@users.noreply.github.com>
2022-10-24 22:51:28 -0400

    Update build_pqr_data.py

--- research/deeplab/datasets/build_pqr_data.py
+++ research/deeplab/datasets/build_pqr_data.py
@@ -64,22 +64,22 @@ import tensorflow as tf
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('image_folder',
-                           './deeplab/datasets/PQR/JPEGImages',
+                           './PQR/JPEGImages',
                            'Folder containing images.')
 
 tf.app.flags.DEFINE_string(
     'semantic_segmentation_folder',
-    './deeplab/datasets/PQR/SegmentationClassRaw',
+    './PQR/SegmentationClassRaw',
     'Folder containing semantic segmentation annotations.')
 
 tf.app.flags.DEFINE_string(
     'list_folder',
-    './deeplab/datasets/PQR/ImageSets',
+    './PQR/ImageSets',
     'Folder containing lists for training and validation')
 
 tf.app.flags.DEFINE_string(
     'output_dir',
-    './deeplab/datasets/PQR/tfrecord',
+    './PQR/tfrecord',
     'Path to save converted SSTable of TensorFlow examples.')
 
 
